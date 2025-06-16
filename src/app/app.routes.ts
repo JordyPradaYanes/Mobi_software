5f41ba12908bbf9c21f8fd1700fa59fe9ba34ac0
JordyPradaYanes <jdpraday@ufpso.edu.co>
2025-06-16 18:10:12 -0500

    updatefiles1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,12 @@ import { LoginComponent } from './ComponentesEstructurales/login/login.component
 import { RegisterComponent } from './ComponentesEstructurales/register/register.component';
 import { PropertyBenefitsComponent } from './ComponentesEstructurales/property-benefits/property-benefits.component';
 import { LocationPropertiesComponent } from './ComponentesEstructurales/location-properties/location-properties.component';
+import { DashboardComponent } from './LayoutPriv/dashboard/dashboard.component';
+import { PropertyCardComponent } from './LayoutPriv/property-card/property-card.component';
+import { PropertyFormComponent } from './LayoutPriv/property-form/property-form.component';
+import { FavoritesComponent } from './LayoutPriv/favorites/favorites.component';
+import { PropertyManagementComponent } from './LayoutPriv/property-management/property-management.component';
+import { UserProfileComponent } from './LayoutPriv/user-profile/user-profile.component';
 
 // import { DashboardComponent } from './ComponentesEstructurales/dashboard/dashboard.component';
 
@@ -21,6 +27,14 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: 'property-benefits', component: PropertyBenefitsComponent},
-    { path: 'location-properties', component: LocationPropertiesComponent}
+    { path: 'location-properties', component: LocationPropertiesComponent},
+
+    //LayoutPriv
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'property-card', component: PropertyCardComponent},
+    { path: 'property-form', component: PropertyFormComponent},
+    { path: 'favorites', component: FavoritesComponent},
+    { path: 'property-management', component: PropertyManagementComponent},
+    { path: 'user-profile', component: UserProfileComponent}
 ];
 
